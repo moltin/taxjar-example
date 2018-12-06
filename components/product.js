758996adc25b0bc290b6bbc4aa3939f61e2242cb
Marc Qualie <marc@marcqualie.com>
2018-12-06 14:05:25 +0000

    Shuffle card layout so elements are clickable

--- components/product.js
+++ components/product.js
@@ -4,19 +4,17 @@ class Product extends Component {
   render() {
     const { id, description, meta, main_image, name, sku, tax_code } = this.props
 
-    return <div key={id} className="card w-75">
+    return <div key={id} className="card">
       <img className="card-img-top" src={main_image.link.href} alt={name} />
-      <div className="card-img-overlay">
-        <h3 className="card-title text-light">{name}</h3>
-      </div>
       <div className="card-body">
+        <h3 className="card-title">{name}</h3>
         <p className="card-text">{description}</p>
         <p className="text-muted">SKU: {sku}<br />Tax Code: {tax_code}</p>
       </div>
       <div className="card-footer">
-        <a href="#" className="btn btn-success">
+        <button className="btn btn-primary">
           Buy now for {meta.display_price.without_tax.formatted}
-        </a>
+        </button>
       </div>
     </div>
   }
