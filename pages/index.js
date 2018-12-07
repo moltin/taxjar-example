b3d87f7c4ee01e7eb515b9b0c17f00c28e4c5a5e
Marc Qualie <marc@marcqualie.com>
2018-12-07 18:08:36 +0000

    eslint --fix .

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,20 @@ import Product from '../components/product'
 import { loadProducts } from '../store'
 
 class ProductsPage extends Component {
-
   componentDidMount() {
     this.props.dispatch(loadProducts())
   }
-  
+
   render() {
     const { products } = this.props
 
-    return <div className="card-deck">
-      {products.map(product => <Product key={product.id} {...product}/>)}
-    </div>
+    return (
+      <div className="card-deck">
+        {products.map(product => <Product key={product.id} {...product} />)}
+      </div>
+    )
   }
 }
 
-const mapStateToProps = (state) => ({ products: state.products })
+const mapStateToProps = state => ({ products: state.products })
 export default connect(mapStateToProps)(ProductsPage)
--- store.js
+++ store.js
@@ -25,7 +25,7 @@ export const reducer = (state = initialState, action) => {
     case actionTypes.SET_CART_ITEMS:
       return {
         ...state,
-        cartItems: action.items
+        cartItems: action.items,
       }
     default:
       return state
@@ -35,8 +35,8 @@ export const reducer = (state = initialState, action) => {
 export const loadProducts = () => async (dispatch) => {
   const products = await client.get('products?include=main_images')
 
-  products.data.map(product => {
-    products.included.main_images.map(image => {
+  products.data.map((product) => {
+    products.included.main_images.map((image) => {
       if (image.id == product.relationships.main_image.data.id) {
         product.main_image = image
       }
@@ -47,28 +47,23 @@ export const loadProducts = () => async (dispatch) => {
 }
 
 export const addToCart = (cartId, productId) => async (dispatch) => {
-
-  let { data } = await client.post(`carts/${cartId}/items`, {
+  const { data } = await client.post(`carts/${cartId}/items`, {
     id: productId,
     type: 'cart_item',
-    quantity: 1
+    quantity: 1,
   })
 
   dispatch(setCartItems(data))
 }
 
-export const loadCart = (cartId) => async (dispatch) => {
+export const loadCart = cartId => async (dispatch) => {
   const { data } = await client.get(`carts/${cartId}/items`)
   dispatch(setCartItems(data))
 }
 
-export const setProducts = (products) => {
-  return { type: actionTypes.SET_PRODUCTS, products }
-}
+export const setProducts = products => ({ type: actionTypes.SET_PRODUCTS, products })
 
-export const setCartItems = (items) => {
-  return { type: actionTypes.SET_CART_ITEMS, items: items }
-}
+export const setCartItems = items => ({ type: actionTypes.SET_CART_ITEMS, items })
 
 export function initializeStore(initialStoreState = initialState) {
   if (initialStoreState.cartId === null) {
