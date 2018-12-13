111cab9b5443195a840af3cded4e25489f466376
Andrew Waters <waters@me.com>
2018-12-13 11:20:17 +0000

    Enable await in for loops and split out functionality to function calls

--- server/express.js
+++ server/express.js
@@ -3,7 +3,7 @@ const { createClient } = require('@moltin/request')
 const taxclient = require('../lib/taxjar')
 require('dotenv').config()
 
-const client = new createClient({
+const client = new createClient({ // eslint-disable-line new-cap
   client_id: process.env.MOLTIN_CLIENT_ID,
   client_secret: process.env.MOLTIN_CLIENT_SECRET,
 })
@@ -16,106 +16,114 @@ const fromState = process.env.FROM_STATE
 const fromCity = process.env.FROM_CITY
 const fromStreet = process.env.FROM_STREET
 
+const deleteTax = async (cartId, itemId, taxId) => { // eslint-disable-line arrow-body-style
+  return client.delete(`carts/${cartId}/items/${itemId}/taxes/${taxId}`)
+}
 
-// Get our tax jar calculations
-router.post('/calculate', async (req, res) => {
-  const taxJarRequest = {
+const createTax = async (cartId, itemId, taxObject) => { // eslint-disable-line arrow-body-style
+  return client.post(`carts/${cartId}/items/${itemId}/taxes`, taxObject)
+}
 
+const getExistingTaxes = async (cartId) => {
+  const taxes = []
+  const { data } = await client.get(`carts/${cartId}/items`)
+  data.forEach((item) => {
+    try {
+      item.relationships.taxes.data.forEach((taxRel) => {
+        taxes.push({
+          itemId: item.id,
+          id: taxRel.id,
+        })
+      })
+    } catch (e) {
+      console.log('No tax on item', item.id)
+    }
+  })
+  return taxes
+}
+
+const buildTaxJarRequest = (address, items) => {
+  const request = {
     from_country: fromCountry,
     from_zip: fromZip,
     from_state: fromState,
     from_city: fromCity,
     from_street: fromStreet,
 
-    to_city: req.body.city,
-    to_state: req.body.jurisdiction,
-    to_country: req.body.country,
-    to_zip: req.body.zip,
+    to_city: address.city,
+    to_state: address.jurisdiction,
+    to_country: address.country,
+    to_zip: address.zip,
 
     shipping: 0,
     line_items: [],
   }
-  req.body.cartItems.forEach((cartItem) => {
+
+  // note that we have hard coded our currency exponent against base 10 here
+  // unit_price in TaxJar is different to Moltin's unit (pennies vs pounds)
+  items.forEach((cartItem) => {
     const taxItem = {
       id: cartItem.id,
       quantity: cartItem.quantity,
-      unit_price: cartItem.unit_price.amount / 100, // note that we have hard coded our exponent against base 10 here
+      unit_price: cartItem.unit_price.amount / 100,
       product_tax_code: cartItem.tax_code,
     }
-    taxJarRequest.line_items.push(taxItem)
+    request.line_items.push(taxItem)
   })
 
-  const taxJarResponse = await taxclient.taxForOrder(taxJarRequest).catch((err) => {
-    res.status(err.status).send({
-      errors: [
-        {
-          message: err.detail,
-        },
-      ],
-    })
-  })
+  return request
+}
 
-  res.status(200).send(taxJarResponse)
-})
-
-// Delete all of our taxes
+// Delete all of our existing taxes in this cart
 router.delete('/tax', async (req, res) => {
   const { cartId } = req.body
-  const existingTaxes = await client.get(`carts/${cartId}/items`).catch((err) => {
-    res.status(400).send({
-      errors: [
-        {
-          message: err.errors,
-        },
-      ],
-    })
-  })
+  const existingTaxes = await getExistingTaxes(cartId)
 
-  await existingTaxes.data.forEach(async (cartItem) => {
-    if (
-      typeof cartItem.relationships !== 'undefined'
-        && typeof cartItem.relationships.taxes !== 'undefined'
-        && typeof cartItem.relationships.taxes.data !== 'undefined'
-    ) {
-      await cartItem.relationships.taxes.data.forEach(async (taxRel) => {
-        const response = await client.delete(`carts/${cartId}/items/${cartItem.id}/taxes/${taxRel.id}`).catch((err) => {
-          res.status(400).send({
-            errors: [
-              {
-                message: err.errors,
-              },
-            ],
-          })
-        })
-        console.log(`Status? - ${response}`)
-        console.log(`Tax delete: carts/${cartId}/items/${cartItem.id}/taxes/${taxRel.id}`)
-      })
-    }
-  })
+  for (let i = 0; i < existingTaxes.length; i += 1) {
+    const tax = existingTaxes[i]
+    await deleteTax(cartId, tax.itemId, tax.id) // eslint-disable-line no-await-in-loop
+    console.log('Deleted tax', tax.id)
+  }
+
   res.status(200).send({})
 })
 
-// Apply tax to our cart based off tax jar response
+// Get our tax jar calculations
+router.post('/calculate', async (req, res) => {
+  const taxJarRequest = buildTaxJarRequest(
+    {
+      city: req.body.city,
+      jurisdiction: req.body.jurisdiction,
+      zip: req.body.zip,
+      country: req.body.country,
+    },
+    req.body.cartItems,
+  )
+
+  const taxJarResponse = await taxclient.taxForOrder(taxJarRequest)
+  res.status(200).send(taxJarResponse)
+})
+
+// Apply tax to our cart based off TaxJar response
 router.post('/tax', async (req, res) => {
-  const { taxJarResponse, cartItems } = req.body
-  // if line_items is empty, it means there are no taxes applicable to the cart
-  if (taxJarResponse.data.tax.breakdown !== undefined) {
+  const { taxJarResponse, cartItems, cartId } = req.body
+
+  try {
     const taxItems = taxJarResponse.data.tax.breakdown.line_items
-    await taxItems.forEach(async (taxItem) => {
-      const cartItem = cartItems.filter(item => item.id === taxItem.id)
-      const moltinTaxItem = {
+
+    for (let i = 0; i < taxItems.length; i += 1) {
+      const tax = taxItems[i]
+      const cartItem = cartItems.filter(item => item.id === tax.id).shift()
+      await createTax(cartId, tax.id, { // eslint-disable-line no-await-in-loop
         type: 'tax_item',
         name: 'Item Tax',
         jurisdiction: taxJarResponse.data.tax.jurisdictions.country,
-        code: cartItem[0].tax_code,
+        code: cartItem.tax_code,
         rate: taxJarResponse.data.tax.rate,
-      }
-      const { statusCode } = await client.post(`carts/${req.body.cartId}/items/${taxItem.id}/taxes`, moltinTaxItem).catch(console.error)
-      // console.log('Created moltin tax item:', data)
-      console.log('status for tax create:', statusCode)
-    })
-  } else {
-    console.log('TaxJar responded without breakdown, no moltin taxt items created')
+      })
+    }
+  } catch (e) {
+    console.log('No tax to apply to this cart')
   }
 
   res.status(200).send({})
